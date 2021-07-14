c0cb8da84eb77f397e24f07655aade7392afeaa7
tto <torsten.thau@cape-it.de>
2021-07-14 13:33:51 +0200

    updated copyright year

--- bin/kix18.CSVSync.pl
+++ bin/kix18.CSVSync.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 # --
 # bin/kix18.dataimport.pl - imports CSV data into KIX18
-# Copyright (C) 2006-2020 c.a.p.e. IT GmbH, http://www.cape-it.de/
+# Copyright (C) 2006-2021 c.a.p.e. IT GmbH, http://www.cape-it.de/
 #
 # written/edited by:
 # * Torsten(dot)Thau(at)cape(dash)it(dot)de
